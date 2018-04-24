674846ddaa594a21a0512b179f334b304c86fd35
dattran <dattran1997@gmail.com>
2018-04-24 17:57:21 +0700

    homework session5

--- Session5/homework/ex_11.py
+++ Session5/homework/ex_11.py
@@ -2,13 +2,17 @@ px =1
 py =1
 pxb =2
 pyb =2
+p = {'x':1,'y':1}
+pb = {'x':2,'y':2}
+
+
 loop=True
 while loop:
     for y in range(4):
         for x in range(4):
-            if (x == px and y == py):
+            if (x == p['x'] and y == p['y']):
                 print("P",end=" ")
-            elif(x == pxb and y == pyb ):
+            elif(x == pb['x'] and y == pb['y'] ):
                 print("B",end=" ")
             elif(x == 1 and y == 3):
                 print("G",end=" ")
@@ -17,10 +21,10 @@ while loop:
         print()
 
     option = input("your move ? W/A/S/D").upper()
-    next_px = px
-    next_py = py
-    next_pxb = pxb
-    next_pyb = pyb
+    next_px = p['x']
+    next_py = p['y']
+    next_pxb = pb['x']
+    next_pyb = pb['y']
     if (option == "D"):
         next_px += 1
     elif(option == "A"):
@@ -31,7 +35,7 @@ while loop:
         next_py -= 1
 
 
-    if(pxb == next_px and pyb == next_py):
+    if(pb['x'] == next_px and pb['y'] == next_py):
         if (option == "D"):
             next_pxb = next_pxb + 1
         elif(option == "A"):
@@ -42,17 +46,17 @@ while loop:
             next_pyb = next_pyb - 1
 
     if(0<= next_pxb <4):
-        pxb = next_pxb
+        pb['x'] = next_pxb
 
     if(0<= next_pyb <4):
-        pyb = next_pyb
+        pb['y'] = next_pyb
 
     if(0<= next_px <4):
-        px = next_px
+        p['x'] = next_px
 
     if(0<= next_py <4):
-        py = next_py
+        p['y'] = next_py
 
-    if( pxb == 1 and pyb == 3):
+    if( pb['x'] == 1 and pb['y'] == 3):
         print("congrat!")
         loop = False
--- Session5/homework/ex_12.py
+++ Session5/homework/ex_12.py
@@ -2,13 +2,17 @@ px =1
 py =1
 pxb =2
 pyb =2
-loop =True
+p = {'x':1,'y':1}
+pb = {'x':2,'y':2}
+
+
+loop=True
 while loop:
     for y in range(4):
         for x in range(4):
-            if (x == px and y == py):
+            if (x == p['x'] and y == p['y']):
                 print("P",end=" ")
-            elif(x == pxb and y == pyb ):
+            elif(x == pb['x'] and y == pb['y'] ):
                 print("B",end=" ")
             elif(x == 1 and y == 3):
                 print("G",end=" ")
@@ -17,44 +21,40 @@ while loop:
         print()
 
     option = input("your move ? W/A/S/D").upper()
-    next_px = px
-    next_py = py
-    next_pxb = pxb
-    next_pyb = pyb
+    next_px = p['x']
+    next_py = p['y']
+    next_pxb = pb['x']
+    next_pyb = pb['y']
     dx=0
     dy=0
     if (option == "D"):
-        #next_px += 1
         dx = 1
     elif(option == "A"):
-        #next_px -= 1
         dx = -1
     elif(option == "S"):
-        #next_py += 1
         dy = 1
     elif(option == "W"):
-        #next_py -= 1
-        dy =-1
+        dy = -1
 
     next_px = next_px + dx
     next_py = next_py + dy
 
-    if(pxb == next_px and pyb == next_py):
+    if(pb['x'] == next_px and pb['y'] == next_py):
         next_pxb = next_pxb + dx
         next_pyb = next_pyb + dy
 
     if(0<= next_pxb <4):
-        pxb = next_pxb
+        pb['x'] = next_pxb
 
     if(0<= next_pyb <4):
-        pyb = next_pyb
+        pb['y'] = next_pyb
 
     if(0<= next_px <4):
-        px = next_px
+        p['x'] = next_px
 
     if(0<= next_py <4):
-        py = next_py
+        p['y'] = next_py
 
-    if( pxb == 1 and pyb == 3):
+    if( pb['x'] == 1 and pb['y'] == 3):
         print("congrat!")
         loop = False
