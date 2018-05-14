54910414364863f332fa06ef933832f370a1ca06
dattran <dattran1997@gmail.com>
2018-05-15 06:29:23 +0700

    homework web1

--- web1/homework/ex1.py
+++ web1/homework/ex1.py
@@ -2,10 +2,18 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/about-me')
+@app.route('/about_me')
 def index():
+    a = {
+        'name':'Trần Đat',
+        'age':'21',
+        'hobbie':'game',
+        'school':'hust'
+    }
+    return render_template('index.html',a = a )
 
-    return render_template('index.html')
-
+@app.route('/school')
+def school():
+    return render_template('school.html')
 if __name__ == '__main__':
   app.run( debug=True)
